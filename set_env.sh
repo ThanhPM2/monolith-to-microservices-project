97ca0d823ea5deb382224cc50c06b418f7c07e6d
ThanhPM <>
2022-11-13 22:11:42 +0700

    add forder kube-config

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cavowscpqynp.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cdfzaqss1zl7.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=thanhpm-project3
 export AWS_REGION=us-east-1
